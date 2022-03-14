73c17ead7934a7a3fc6d0b3a469c04ddc2b22bf8
takker99 <37929109+takker99@users.noreply.github.com>
2022-03-14 11:25:30 +0900

    :art: 今後使いそうなmoduleを準備しておく

---
+++ deps/scrapbox-std.ts
@@ -0,0 +1,26 @@
+export {
+  caret,
+  cursor,
+  editor,
+  getCharDOM,
+  getLines,
+  getText,
+  insertLine,
+  openInTheSameTab,
+  replaceLines,
+  textInput,
+  useStatusBar,
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/browser/dom/mod.ts";
+export type {
+  CaretInfo,
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/browser/dom/mod.ts";
+export {
+  sleep,
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/sleep.ts";
+export {
+  getIndentLineCount,
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/text.ts";
+export {
+  encodeTitleURI,
+  toTitleLc,
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/title.ts";
---
+++ deps/scrapbox.ts
@@ -0,0 +1,1 @@
+export * from "https://raw.githubusercontent.com/scrapbox-jp/types/0.0.8/mod.ts";
