955067d017e1996abd15e26311b510cfa2d14b9a
GitHub <noreply@github.com>
2025-04-16 00:40:38 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-std.ts
+++ deps/scrapbox-std.ts
@@ -10,17 +10,17 @@ export {
   replaceLines,
   textInput,
   useStatusBar,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.12/browser/dom/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.13/browser/dom/mod.ts";
 export type {
   CaretInfo,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.12/browser/dom/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.13/browser/dom/mod.ts";
 export {
   sleep,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.12/sleep.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.13/sleep.ts";
 export {
   getIndentLineCount,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.12/text.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.13/text.ts";
 export {
   encodeTitleURI,
   toTitleLc,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.12/title.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.13/title.ts";
